0dcf7fd0a58114a96c8e05a69848549b1929c731
Eric Lin <80200462+ericlin7@users.noreply.github.com>
2022-01-04 13:41:02 -0800

    Add files via upload

---
+++ code.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+ 
+int main() {
+
+	std::cout << "email either: e44lin@uwaterloo.ca or jy7tian@uwaterloo.ca for source code or a token for private UW Git repository << std::endl
+	std::cout << ":) " << std::endl;
+}
