7554ffce9934f5817bfb5983ac0a26b37db4684a
Eric Lin <80200462+ericlin7@users.noreply.github.com>
2022-01-11 14:07:29 -0800

    Update code.cc

--- code.cc
+++ code.cc
@@ -2,6 +2,6 @@
  
 int main() {
 
-	std::cout << "email either: e44lin@uwaterloo.ca or jy7tian@uwaterloo.ca for source code or a token for private UW Git repository << std::endl
+	std::cout << "email either: e44lin@uwaterloo.ca or jy7tian@uwaterloo.ca for source code or a token for private UW Git repository << std::endl;
 	std::cout << ":) " << std::endl;
 }
